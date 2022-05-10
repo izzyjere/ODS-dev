9c5e868da41cc00be5695c732822a4d50d420019
Wisdom Jere <wisdomjere5@gmail.com>
2022-05-10 20:00:42 +0200

    more interfaces

---
+++ Domain/ODS.Domain/Interfaces/IService.cs
@@ -0,0 +1,17 @@
+﻿namespace ODS.Domain.Interfaces
+{
+    public interface IService
+    {
+    }
+    public interface IService<T, TKey> : IService where T : class, IEntity<TKey>
+    {
+        Task<T> GetById(TKey id);
+        Task<List<T>> GetAll();
+
+        Task<bool> Create(T entity);
+
+        Task<bool> Update(T entity);
+
+        Task<bool> Delete(T entity);
+    }
+}
