015daee468ccf1efbcc0a3dbe91a8c151ba5c795
izzyjere <wisdomjere5@gmail.com>
2022-06-13 11:56:27 +0200

    New components

--- Domain/ODS.Domain/HelperModels/RegisterRequest.cs
+++ Domain/ODS.Domain/HelperModels/RegisterRequest.cs
@@ -8,10 +8,8 @@
         [Required]
         public string FirstName { get; set; }
         [Required]
-        public string LastName { get; set; }
-        [Required]
-        [MinLength(6)]
-        public string UserName { get; set; }
+        public string LastName { get; set; }    
+        public string? UserName { get; set; }
         [Required]
         public string Password { get; set; }
         [Required, Compare(nameof(Password), ErrorMessage = "Password and Confirm Password must match")]
