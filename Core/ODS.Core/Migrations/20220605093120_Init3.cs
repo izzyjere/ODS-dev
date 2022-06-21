ba29dc0367aed0a99b1fde1fbb02f5707f6623fe
izzyjere <wisdomjere5@gmail.com>
2022-06-21 22:15:50 +0200

    add migrations

---
+++ Core/ODS.Core/Migrations/20220605093120_Init3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ODS.Core.Migrations
+{
+    public partial class Init3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrphanageId",
+                schema: "Domain",
+                table: "Donations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrphanageId",
+                schema: "Domain",
+                table: "Donations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ Core/ODS.Core/Migrations/20220615202954_Init4.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ODS.Core.Migrations
+{
+    public partial class Init4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                schema: "Domain",
+                table: "Donors");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "Domain",
+                table: "Donors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                schema: "Domain",
+                table: "Orphanages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                schema: "Domain",
+                table: "Orphanages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                schema: "Domain",
+                table: "Donors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "Domain",
+                table: "Donors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
