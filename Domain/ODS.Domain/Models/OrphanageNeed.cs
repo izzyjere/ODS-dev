3bbeebf3901d9cc9cdb6ed45bd0ebbd34e26d841
izzyjere <wisdomjere5@gmail.com>
2022-07-06 14:06:57 +0200

    dialog

--- Domain/ODS.Domain/Models/OrphanageNeed.cs
+++ Domain/ODS.Domain/Models/OrphanageNeed.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 namespace ODS.Domain.Models
 {
     public class OrphanageNeed : Entity<int>
-    {
+    {   [Required]
         public string Description { get; set; }
         public int OpharnageId { get; set; }
         public virtual Orphanage Orphanage { get; set; }
+        [Required,Range(1,4)]
         public DonationType Type { get; set; }
+        [Required, Range(1, double.MaxValue)]
         public double Target { get; set; }
         public double Raised { get; set; }
         public DateTime CreatedOn { get; set;}
