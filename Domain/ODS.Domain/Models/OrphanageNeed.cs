8b8694b0c4a8feb1f55970e36222a76f7c974ded
izzyjere <wisdomjere5@gmail.com>
2022-07-06 13:41:51 +0200

    needs

--- Domain/ODS.Domain/Models/OrphanageNeed.cs
+++ Domain/ODS.Domain/Models/OrphanageNeed.cs
@@ -13,5 +13,20 @@ namespace ODS.Domain.Models
         public virtual Orphanage Orphanage { get; set; }
         public DonationType Type { get; set; }
         public double Target { get; set; }
+        public double Raised { get; set; }
+        public DateTime CreatedOn { get; set;}
+        public OrphanageNeed()
+        {
+            CreatedOn = DateTime.Now;
+        }
+        public string GetTarget()
+        {
+            return (Type == DonationType.Money) ? Target + " ZMW" : Convert.ToInt32(Target) + " items";
+        }
+        public string GetRaised()
+        {
+            return (Type == DonationType.Money) ? Raised + " ZMW" : Convert.ToInt32(Raised) + " items";
+        }
+
     }
 }
