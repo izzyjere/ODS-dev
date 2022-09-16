2b72569c0bf753252c08cb1906f060b6ebd189fb
izzyjere <wisdomjere5@gmail.com>
2022-09-16 17:13:17 +0200

    add report

--- src/ODS/Services/Domain/OrphanageService.cs
+++ src/ODS/Services/Domain/OrphanageService.cs
@@ -9,6 +9,10 @@
         {
             return await Repository.Entities().Include(d => d.Donations).ThenInclude(d => d.Donor).Include(o => o.OrphanageNeeds).Include(o=>o.Payments).ThenInclude(p=>p.Donor).FirstOrDefaultAsync(x => x.Email == email);
         }
+        public async Task<Orphanage> Get(string id)
+        {
+            return await Repository.Entities().Include(d => d.Donations).ThenInclude(d => d.Donor).Include(o => o.OrphanageNeeds).Include(o => o.Payments).ThenInclude(p => p.Donor).FirstOrDefaultAsync(x => x.Id == Convert.ToInt32(id));
+        }
         public async Task<List<Orphanage>> GetAll()
         {
             return await Repository.Entities().Include(o => o.OrphanageNeeds).Include(o => o.Payments).ThenInclude(p => p.Donor).ToListAsync();
---
+++ src/ODS/wwwroot/assets/js/vendor/fileHelper.js
@@ -0,0 +1,25 @@
+﻿window.tableToExcel = (function () {
+    var uri = 'data:application/vnd.ms-excel;base64,'
+        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head> <!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/><style>{cssStyle}</style></head><body><table>{table}</table></body></html>'
+        , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+        , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
+    return function (table, name, fileName) {
+        if (!table.nodeType) table = document.getElementById(table)
+        var xmlhttp;
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.addEventListener("load", done, false);
+        xmlhttp.open("GET", "assets/css/excel-css.css", true);
+        xmlhttp.send();
+        function done() {
+            var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML, cssStyle: xmlhttp.responseText }
+
+            // window.location.href = uri + base64(format(template, ctx))
+            anchor = document.createElement("a");
+            anchor.href = uri + base64(format(template, ctx));
+            anchor.download = fileName;
+            anchor.click();
+            anchor.remove();
+
+        }
+    }
+})()
